288db25580019a69f9a10fee661146c2a3aa1510
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2020-02-10 07:14:47 +0000

    initial

---
+++ lib/components/fly.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+
+import 'package:flame/sprite.dart';
+import 'package:langaw/langaw-game.dart';
+
+class Fly {
+  final LangawGame game;
+  List<Sprite> flyingSprite;
+  Sprite deadSprite;
+  double flyingSpriteIndex = 0;
+  Rect flyRect;
+  bool isDead = false;
+  bool isOffScreen = false;
+
+  Fly(this.game, double x, double y) {
+    flyRect = Rect.fromLTWH(x, y, game.tileSize, game.tileSize);
+  }
+
+  void render(Canvas c) {
+    if (isDead) {
+      deadSprite.renderRect(c, flyRect.inflate(2));
+    } else {
+      flyingSprite[flyingSpriteIndex.toInt()].renderRect(c, flyRect.inflate(2));
+    }
+  }
+
+  void update(double t) {
+    if (isDead) {
+      flyRect = flyRect.translate(0, game.tileSize * 12 * t);
+      if (flyRect.top > game.screenSize.height) {
+        isOffScreen = true;
+      }
+    }
+  }
+
+  void onTapDown() {
+    isDead = true;
+    game.spawnFly();
+  }
+}
